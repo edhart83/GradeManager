9d9fee3ea3f4792ebbd7d7b5bc11e6363d22d15c
edhart83 <edhart83@gmail.com>
2021-08-31 23:57:06 +0800

    created menu screen and began switch statement

--- GradeManager/Program.cs
+++ GradeManager/Program.cs
@@ -6,7 +6,34 @@ namespace GradeManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            // Display title & menu choices
+            Console.WriteLine("Welcome to Grade Manager\n");
+            Console.WriteLine("Please choose an option below:");
+            Console.WriteLine("1. Show Grades\n" + 
+                                "2. Add Grade\n" + 
+                                "3. Show Class Average\n" + 
+                                "4. Show Best Grade\n" +
+                                "5. Show Worst Grade\n" +
+                                "6. Remove Grade\n" +
+                                "7. Edit Grade\n" +
+                                "8. Exit Application");
+            Console.WriteLine("------------------");
+
+            // Store user's choice into variable
+            int menuChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (menuChoice)
+            {
+                case 1:
+                    Console.WriteLine("You selected option " + menuChoice);
+                    break;
+                default:
+                    break;
+            } // ------------ END OF SWITCH ------------
+
+
+        } // ------------ END OF MAIN ------------
+
+
+    } // ------------ END OF PROGRAM CLASS ------------
+} // ------------ END OF GRADEMANAGER NAMESPACE ------------
